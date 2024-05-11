a6c1d1364103b7e43abab8099a89586ad63801a4
Sumaya Shimu Rima <sumayashimurima@gmail.com>
2024-05-12 01:40:30 +0600

    skeleton of login

--- client/src/components/auth/Login.jsx
+++ client/src/components/auth/Login.jsx
@@ -1,8 +1,30 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <div>
+      <h3>Login</h3>
+        <form>
+          <div>
+            <label>Email:</label>
+            <input type="email"/>
+          </div>
+
+          <div>
+            <label>password:</label>
+            <input type="password" />
+          </div>
+
+          <button> Login </button>
+
+          <div >
+            <p >Do you have an account?</p>
+            <Link to={"/register"}>Register</Link>
+          </div>
+        </form>
+      
+    </div>
   )
 }
 
