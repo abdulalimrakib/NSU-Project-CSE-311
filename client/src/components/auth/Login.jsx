bdc39982c738ff08b7f83f7127c59c33c03870a5
Sumaya Shimu Rima <sumayashimurima@gmail.com>
2024-05-21 20:11:08 +0600

    design login page

--- client/src/components/auth/Login.jsx
+++ client/src/components/auth/Login.jsx
@@ -1,29 +1,72 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import "./login.css"
 
 const Login = () => {
   return (
-    <div>
-      <h3>Login</h3>
-        <form>
-          <div>
-            <label>Email:</label>
-            <input type="email"/>
-          </div>
+    <div className='div'>
+      <div class="container">
+        <img class="image" src="./JobZeelogo.png"></img>
+      </div>
+      <div class="div-clear"></div>
 
-          <div>
+
+      <h1 class="h4">Login to your account</h1>
+
+      <form>
+        <div className='form'>
+
+          <div className="em py-4">
+
+            <label>Email Address:</label>
+            <p><input className='border-b-2 focus:outline-none focus:border-[#00DFC0]' type="email" required />
+            </p>
+          </div>
+          <div class="div-clear"></div>
+
+
+          <div className="em py-4">
             <label>password:</label>
-            <input type="password" />
+            <p className='p'><input className='border-b-2 focus:outline-none focus:border-[#00DFC0]' type="password" required />
+            </p>
           </div>
 
-          <button> Login </button>
+          <div class="div-clear"></div>
 
-          <div >
-            <p >Do you have an account?</p>
-            <Link to={"/register"}>Register</Link>
+          <button type="submit" class="btn">Login</button>
+
+        </div>
+
+        <div class="div-clear"></div>
+
+
+
+        <div>
+          <img class="image1" src="./login.png"></img>
+        </div>
+
+        <div class="div-clear"></div>
+
+
+        <div >
+
+          <div className='em2 py-4'>
+
+            <h3 className='h3'>Do you have an account?</h3>
+
+            <Link to={"/register"} className='li'>Register </Link>
+
           </div>
-        </form>
-      
+
+          <div class="div-clear"></div>
+
+        </div>
+
+        <div class="div-clear"></div>
+
+
+      </form>
+
     </div>
   )
 }
