87364d5c99f304dd7c5d3697fa07aea7b28e89e6
Sumaya Shimu Rima <sumayashimurima@gmail.com>
2024-05-28 14:53:10 +0600

    fixing some error by abdul alim rakib

--- client/src/components/auth/Login.jsx
+++ client/src/components/auth/Login.jsx
@@ -1,12 +1,9 @@
-
-import React from 'react'
-import { Link } from 'react-router-dom'
-import "./login.css"
-import axios from "axios";
 import React, { useState } from "react";
+import axios from "axios";
 import { useContext } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { userContext } from "../../hooks/context";
+import "./login.css"
 
 const Login = () => {
   const [formData, setFormData] = useState({});
@@ -38,7 +35,7 @@ const Login = () => {
 
   return (
     <div className='div'>
-      <div class="container">
+      <div className="container mb-5">
         <img class="image" src="./JobZeelogo.png"></img>
       </div>
       <div class="div-clear"></div>
@@ -46,61 +43,48 @@ const Login = () => {
 
       <h1 class="h4">Login to your account</h1>
 
-      <form>
+      <form onSubmit={postData}>
         <div className='form'>
 
           <div className="em py-4">
 
             <label>Email Address:</label>
-            <p><input className='border-b-2 focus:outline-none focus:border-[#00DFC0]' type="email" required />
+            <p><input type="email"
+              name="email"
+              onChange={handleChange} className='border-b-2 focus:outline-none focus:border-[#00DFC0] mt-3' required />
             </p>
           </div>
           <div class="div-clear"></div>
-
 
           <div className="em py-4">
             <label>password:</label>
-            <p className='p'><input className='border-b-2 focus:outline-none focus:border-[#00DFC0]' type="password" required />
+            <p className='p'><input type="password"
+              name="password"
+              placeholder=""
+              onChange={handleChange} className='border-b-2 focus:outline-none focus:border-[#00DFC0] mt-3' required />
             </p>
           </div>
-
           <div class="div-clear"></div>
-
           <button type="submit" class="btn">Login</button>
-
         </div>
-
         <div class="div-clear"></div>
-
-
-
         <div>
           <img class="image1" src="./login.png"></img>
         </div>
 
         <div class="div-clear"></div>
 
-
         <div >
-
           <div className='em2 py-4'>
-
             <h3 className='h3'>Do you have an account?</h3>
-
             <Link to={"/register"} className='li'>Register </Link>
-
           </div>
-
           <div class="div-clear"></div>
-
         </div>
-
         <div class="div-clear"></div>
-
-
       </form>
-    <div>
-      <h3>Login</h3>
+    </div>
+        /* <h3>Login</h3>
       <form onSubmit={postData}>
         <div>
           <label>Email:</label>
@@ -130,8 +114,8 @@ const Login = () => {
           <Link to={"/register"}>Register</Link>
         </div>
       </form>
-    </div>
-  );
+    </div> */
+        );
 };
 
 export default Login;
